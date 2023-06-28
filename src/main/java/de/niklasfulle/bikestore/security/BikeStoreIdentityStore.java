adf0edb9f78f18fb0977eb9f67696a79c8ca12f5
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-06-29 01:22:46 +0200

    added the logic to login as user

---
+++ src/main/java/de/niklasfulle/bikestore/security/BikeStoreIdentityStore.java
@@ -0,0 +1,67 @@
+package de.niklasfulle.bikestore.security;
+
+import java.util.HashSet;
+import jakarta.inject.Inject;
+import java.util.Collections;
+import jakarta.servlet.http.HttpSession;
+import jakarta.faces.context.FacesContext;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.security.enterprise.credential.Credential;
+import jakarta.security.enterprise.identitystore.IdentityStore;
+import jakarta.security.enterprise.credential.UsernamePasswordCredential;
+import jakarta.security.enterprise.identitystore.CredentialValidationResult;
+
+import de.niklasfulle.bikestore.business.staff.Staff;
+import de.niklasfulle.bikestore.business.staff.StaffService;
+
+/**
+ * BikeStoreIdentityStore implements the IdentityStore interface. It is used to
+ * validate the credentials of the user.
+ */
+@ApplicationScoped
+public class BikeStoreIdentityStore implements IdentityStore {
+  @Inject
+  private StaffService staffService;
+
+  /**
+   * Validates the credentials of the user. It uses the StaffService to fetch the
+   * Staff from the database. If the Staff is not found, the result is invalid.
+   * Otherwise, the password is compared to the password of the Staff. If the
+   * password is correct, the result is valid. Otherwise, the result is invalid.
+   * 
+   * @param credential the credential to be validated
+   * @return the result of the validation
+   */
+  @Override
+  public CredentialValidationResult validate(Credential credential) {
+    UsernamePasswordCredential usernamePasswordCredential = (UsernamePasswordCredential) credential;
+
+    Staff staff;
+
+    // If the Staff is not found, the result is invalid
+    try {
+      staff = staffService.getStaffByEmail(usernamePasswordCredential.getCaller());
+    } catch (Exception e) {
+      return CredentialValidationResult.INVALID_RESULT;
+    }
+
+    // If the password is correct, the result is valid
+    if (((UsernamePasswordCredential) credential).compareTo(staff.getEmail(),
+        staff.getPhone().split(" ")[1])) {
+      HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+
+      String role = staff.getRole();
+
+      session.setAttribute("firstName", staff.getFirstName());
+      session.setAttribute("lastName", staff.getLastName());
+      session.setAttribute("staffEmail", staff.getEmail());
+      session.setAttribute("role", role);
+
+      return new CredentialValidationResult(staff.getRole(), new HashSet<>(
+          Collections.singleton(role)));
+    }
+
+    // Otherwise, the result is invalid
+    return CredentialValidationResult.INVALID_RESULT;
+  }
+}
---
+++ src/main/java/de/niklasfulle/bikestore/view/LoginController.java
@@ -0,0 +1,170 @@
+package de.niklasfulle.bikestore.view;
+
+import java.io.IOException;
+import jakarta.inject.Named;
+import jakarta.inject.Inject;
+import jakarta.servlet.http.HttpSession;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.ExternalContext;
+import jakarta.validation.constraints.NotNull;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.security.enterprise.SecurityContext;
+import jakarta.security.enterprise.credential.Password;
+import jakarta.security.enterprise.AuthenticationStatus;
+import jakarta.security.enterprise.credential.Credential;
+import jakarta.security.enterprise.credential.UsernamePasswordCredential;
+import jakarta.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
+
+import de.niklasfulle.bikestore.business.staff.StaffService;
+
+/**
+ * LoginController handles the login form. It is used to authenticate the user.
+ */
+@Named
+@RequestScoped
+public class LoginController {
+  @Inject
+  private SecurityContext securityContext;
+
+  @Inject
+  private FacesContext facesContext;
+
+  @Inject
+  private StaffService staffService;
+
+  @NotNull
+  private String email;
+
+  @NotNull
+  private String phone;
+
+  /**
+   * Handles current login form
+   */
+  public void login() {
+    Credential credential = new UsernamePasswordCredential(email, new Password(phone));
+    ExternalContext externalContext = facesContext.getExternalContext();
+
+    AuthenticationStatus status = securityContext.authenticate((HttpServletRequest) externalContext.getRequest(),
+        (HttpServletResponse) externalContext.getResponse(),
+        AuthenticationParameters.withParams().credential(credential));
+
+    // Redirects the user to the index page if the login was successful
+    switch (status) {
+      case SEND_CONTINUE:
+        facesContext.responseComplete();
+        break;
+      case SEND_FAILURE, NOT_DONE:
+        break;
+      case SUCCESS:
+        try {
+          getExternalContext().redirect(
+              getExternalContext().getRequestContextPath() + "/index.xhtml");
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+        break;
+    }
+  }
+
+  private ExternalContext getExternalContext() {
+    return facesContext.getExternalContext();
+  }
+
+  /**
+   * Logs the user out
+   * 
+   * @return the index page
+   */
+  public String logout() {
+    FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+    return "View.xhtml?faces-redirect=true";
+  }
+
+  /**
+   * Gets the current http session
+   * 
+   * @return the current http session
+   */
+  private HttpSession getHttpSession() {
+    return (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+  }
+
+  /**
+   * Checks if the user is logged in
+   * 
+   * @return true if the user is logged in, false otherwise
+   */
+  public boolean getLoginStatus() {
+    return getStaffRole() != null;
+  }
+
+  /**
+   * Gets the email of the logged in user
+   * 
+   * @return the email of the logged in user
+   */
+  public String getStaffEmail() {
+    if (getHttpSession() == null) {
+      return null;
+    }
+    Object staffEmail = getHttpSession().getAttribute("staffEmail");
+    if (staffEmail != null) {
+      return staffEmail.toString();
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Gets the role of the logged in user
+   * 
+   * @return the role of the logged in user
+   */
+  public String getStaffRole() {
+    if (getHttpSession() == null) {
+      return null;
+    }
+    Object staffRole = getHttpSession().getAttribute("role");
+    if (staffRole != null) {
+      return staffRole.toString();
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Gets the name of the logged in user
+   * 
+   * @return the name of the logged in user
+   */
+  public String getStaffName() {
+    if (getHttpSession().getAttribute("firstName") != null
+        && getHttpSession().getAttribute("lastName") != null) {
+      return getHttpSession().getAttribute("firstName") + " " + getHttpSession().getAttribute(
+          "lastName");
+    } else {
+      return "";
+    }
+  }
+
+  // Getter and Setters
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+}
